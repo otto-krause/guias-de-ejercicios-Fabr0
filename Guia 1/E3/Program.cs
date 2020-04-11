6b9fe1d6efcee8f649209df3b5fe873c22f3c8d6
Fabr0 <63239900+Fabr0@users.noreply.github.com>
2020-04-11 18:53:25 -0300

    Mejorable
    Declarando las funciones fuera del main me saltaba un error.
    Es medio rustico el codigo, estoy seguro de que se puede hacer mas "limpio"

--- Guia 1/E3/Program.cs
+++ Guia 1/E3/Program.cs
@@ -1,12 +1,58 @@
-﻿using System;
-
-namespace E3
+using System;
+//ver si el numero es par y si el numero es primo.
+namespace Visual
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        
+        int primo (int num1)
+        {
+            int cont=0;
+            for (int i = 1; i <=num1; i++)
+            {
+                 if(num1%i==0)
+                 {
+                     cont++;
+                 }
+            }
+            return cont;
+    
+        }
+        int par (int num1)
+        {
+            if(num1%2==0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+           int num1=0;
+    
+           Console.WriteLine("Ingrese un numero");
+           num1 = Int32.Parse(Console.ReadLine());
+           if(primo(num1)==2)
+           {
+               Console.WriteLine("El numero es primo");
+           }
+           else
+           {
+               Console.WriteLine("El numero no es primo");
+           }
+           if(par(num1)==1)
+           {
+               Console.WriteLine("El numero es par");
+           }
+           else
+           {
+               Console.WriteLine("El numero es impar");
+           }
+  
         }
     }
 }
