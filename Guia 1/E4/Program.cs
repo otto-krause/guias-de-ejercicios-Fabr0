c026ee2acb58d761151edb1f52d594bcec384306
Fabr0 <63239900+Fabr0@users.noreply.github.com>
2020-04-11 20:00:39 -0300

    Update Program.cs

--- Guia 1/E4/Program.cs
+++ Guia 1/E4/Program.cs
@@ -1,12 +1,51 @@
-﻿using System;
+using System;
 
-namespace E4
+namespace Visual
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        int num=0;
+        int min=0;
+            Gatito garfield = new Gatito("Garfield",true,50);
+            while (num !=6)
+            {
+                  Console.WriteLine("Escriba el numero para ejecutar el mensaje:\n1-Nombre\n2-EstaVacunado\n3-Jugar\n4-Comer\n5-EstaSaludable\n6-Finalizar");
+            num=Int32.Parse(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine(garfield.Nombre());
+                    break;
+                case 2:
+                    Console.WriteLine(garfield.EstaVacunado());
+                    break;
+                case 3:
+                    Console.WriteLine("Ingrese los minutos que jugo");
+                    min= Int32.Parse(Console.ReadLine());
+                    garfield.Jugar(min);
+                    break;
+                case 4:
+                    garfield.Comer();
+                    break;
+                case 5:
+                    if(garfield.EstaSaludable())
+                        Console.WriteLine("Si");
+                    else
+                        Console.WriteLine("No");
+                    break;
+          
+                case 6:
+                    break;
+               
+
+                    
+            }
+ 
+            }
+         
         }
     }
 }
