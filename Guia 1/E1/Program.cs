e03430dbf1a029cb256a3e27eef59a4fe5aff966
Fabr0 <63239900+Fabr0@users.noreply.github.com>
2020-04-11 18:14:38 -0300

    Update Program.cs

--- Guia 1/E1/Program.cs
+++ Guia 1/E1/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+using System;
 
-namespace E1
+namespace Visual
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vector = new int[10];
+            int[] ordenado = new int[10];
+            int suma=0;
+            int aux=0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Ingrese un numero");
+                vector[i] = Int32.Parse(Console.ReadLine());
+                suma+=vector[i];
+            }       
+            for (int i = 0; i <(10-1); i++)
+            {
+                for (int k = i+1; k < 10; k++)
+                {
+                    if (vector[i]<vector[k])
+                    {
+                        aux=vector[i];
+                        vector[i]=vector[k];
+                        vector[k]=aux;
+                    }
+                    
+                }
+            }   
+             Console.WriteLine("La suma es: " + suma);
+             Console.WriteLine("El promedio es: " + suma/10);
+             Console.WriteLine("El menor elemento es: "+ vector[9]);
+             for (int i = 0; i < 10; i++)
+             {
+                 Console.WriteLine(vector[i]);
+             }
         }
     }
 }
