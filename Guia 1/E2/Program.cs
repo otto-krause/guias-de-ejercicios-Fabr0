b1aeba7c8075aab2c9fffc55e71c2a960f2e61f0
Fabr0 <63239900+Fabr0@users.noreply.github.com>
2020-04-11 18:33:25 -0300

    Update Program.cs

--- Guia 1/E2/Program.cs
+++ Guia 1/E2/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+using System;
 
-namespace E2
+namespace Visual
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           int num1=0;
+           int num2=0;
+           int pro=0;
+           int cos=0;
+           int k=0;
+           Console.WriteLine("Ingrese dos numeros");
+           k=num1 = Int32.Parse(Console.ReadLine());
+           num2 = Int32.Parse(Console.ReadLine());
+           for (int i=1; i <=num2 ; i++)
+           {
+               pro+=num1;
+               if (k>=num2)
+               {
+                   k-=num2;
+                   cos +=1;
+                   
+               }
+           }
+           Console.WriteLine("El producto de los numeros es :"+pro);
+           Console.WriteLine("El cociente entre los numeros es "+cos+" y tiene resto "+k);
         }
     }
 }
+
